772eaf68f632364f3325285c79a31e8617298f9b
brycepanza <bep236@nau.edu>
2025-12-30 13:09:11 -0700

    Implemented Challenge 1 for the end of Chapter 8 to allow for execution of expressions in the REPL execution mode to be displayed to monitor. I'm not sure if I like this implementation, since I am not super clear on if I maintained proper ownership concepts. In this execution logic, the interpreter has a state variable runningRepl that is set by the caller. This is only ever used by the visitExpressionStmt() method to check if it should log the result of the expression or keep it silent. May revert this commit to keep clean. I didn't want to change the constructor, especially since it feels weird to include this execution mode. I could have made an enum for execution modes, but I won't

--- lox/Interpreter.java
+++ lox/Interpreter.java
@@ -18,6 +18,9 @@ class Interpreter implements Expr.Visitor<Object>,
         // interpreter ownership of scoping maintained while interpreter is running
     private Environment environment = new Environment();
 
+    // state variable set by caller
+    public boolean runningRepl = false;
+
     // public api interface - takes statements and applies interpreter's functionality
     void interpret(List<Stmt> statements) {
         // attempt to evaluate given expression
@@ -190,7 +193,11 @@ class Interpreter implements Expr.Visitor<Object>,
     @Override
     public Void visitExpressionStmt(Stmt.Expression stmt) {
         // generic evaluation call to inner expression
-        evaluate(stmt.expression);
+        Object eval = evaluate(stmt.expression);
+
+        // check if interpreter running in cmdlin, log expression
+        if (runningRepl) System.out.println(stringify(eval));
+
         // statements produce no values
         return null;
     }
