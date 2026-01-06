4f2b3880bb7fc0b80c8a11b8d2c34b86d076d0c8
brycepanza <bep236@nau.edu>
2026-01-06 10:39:43 -0700

    Token structure and basic scanning flow

--- c/compiler.c
+++ c/compiler.c
@@ -13,5 +13,25 @@
 #include "scanner.h"
 
 void compile(const char *source_code) {
+    // apply default state to global scanner
     init_scanner(source_code);
+    int line = 1;
+
+    for (;;) {
+        // get current token
+        Token token = scan_token();
+        // check for next-line found
+        if (line != token.line) {
+            printf("%4d ", token.line);
+            line = token.line;
+        }
+        // default display
+        else printf("\t| ");
+
+        // log token
+        printf("%2d '%.*s'\n", token.type, token.length, token.start);
+
+        // check for exit
+        if (token.type == TOKEN_EOF) break;
+    }
 }
--- c/scanner.c
+++ c/scanner.c
@@ -28,3 +28,46 @@ void init_scanner(const char *source_code) {
     scanner.current = source_code;
     scanner.line = 1;
 }
+
+// helper to determine if at end of scanner's string
+static bool is_at_end() {
+    return *scanner.current == '\0';
+}
+
+// helper to create a new token with scanner's current state
+Token make_token(TokenType type) {
+    Token new_token;
+    // type assigned by caller
+    new_token.type = type;
+    // assign lexeme data by scanner state
+    new_token.start = scanner.start;        // start of lexeme
+    new_token.length = (int)(scanner.current - scanner.start);
+    new_token.line = scanner.line;
+
+    return new_token;
+}
+
+// helper to generate an error token
+static Token error_token(const char *message) {
+    Token token;
+
+    token.type = TOKEN_ERROR;
+    // point to given message - outside of scanner
+    token.start = message;
+    token.length = (int)strlen(message);
+    token.line = scanner.line;
+
+    return token;
+}
+
+// attempt scan next token with scanner's string state
+Token scan_token() {
+    // adjust pointer to correct position
+    scanner.start = scanner.current;
+
+    // check for reached end of file
+    if (is_at_end()) return make_token(TOKEN_EOF);
+
+    // assume error
+    return error_token("Unexpected character.");
+}
