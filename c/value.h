c2b0028cc6b67342ebef90ea656cd4ac89f50ea0
brycepanza <bep236@nau.edu>
2026-01-10 09:10:51 -0700

    Changed Value struct to recognize nil, number, and bool types. Macros for type checking and conversions

--- c/value.h
+++ c/value.h
@@ -11,7 +11,36 @@
 
 #include "common.h"
 
-typedef double Value;
+// value types recognized
+typedef enum {
+    VAL_BOOL,
+    VAL_NIL,
+    VAL_NUMBER
+} ValueType;
+
+// values as tagged unions
+typedef struct {
+    ValueType type;     // tag for recognized type
+    union {
+        bool boolean;
+        double number;
+    } as;
+} Value;
+
+// comparison macros for manual type checking - requires Value struct as parameter
+#define IS_BOOL(value)      ((value).type == VAL_BOOL)
+#define IS_NIL(value)       ((value).type == VAL_NIL)
+#define IS_NUMBER(value)    ((value).type == VAL_NUMBER)
+
+// macros for recognizing union payload type for value extraction
+#define AS_BOOL(value)      ((value).as.boolean)
+#define AS_NUMBER(value)    ((value).as.number)
+
+// macros for assigning and casting proper shape - no type checking
+    // type and value conversions happen together - valid type state
+#define BOOL_VAL(value)     ((Value){VAL_BOOL, {.boolean = value}})
+#define NIL_VAL             ((Value){VAL_NIL, {.number = 0}})
+#define NUMBER_VAL(value)   ((Value){VAL_NUMBER, {.number = value}})
 
 // dynamic array for Lox values
 typedef struct {
